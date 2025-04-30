06e0bf86cb9529f0693d6cca196af66e2d7f9107
test-master3 <247.sunsunsunday.master@gmail.com>
2025-04-30 18:44:37 +0900

    新規作成画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root to: "posts#index" # トップ＝投稿一覧
   resources :posts, only: [:index, :new, :create, :show]
   resources :users, only: [:edit, :update]
+  resources :rooms, only: [:new, :create]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_04_28_123437) do
+ActiveRecord::Schema[7.1].define(version: 2025_04_30_091340) do
   create_table "posts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title"
     t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "rooms", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
