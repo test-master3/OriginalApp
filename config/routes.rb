386abc9dcf054c5da14c29a3a16b9aa5c9c860cf
test-master3 <247.sunsunsunday.master@gmail.com>
2025-05-02 21:10:36 +0900

    調整

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "posts#index" # トップ＝投稿一覧
+  root to: "rooms#index" # トップ＝投稿一覧
   resources :posts, only: [:index, :new, :create, :show]
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:new, :create]
-  resources :rooms, only: [:new, :create] do
+  resources :rooms, only: [:new, :create, :index] do
     resources :messages, only: [:index, :create]
   end
 end
